8ae359229eabc963bdbdd0067ad139fb7833110b
CristhianSG <cristhiansg46@gmail.com>
2022-05-24 18:24:32 -0500

    Agregando campos al endpoint para crear productos

--- src/addProduct.js
+++ src/addProduct.js
@@ -1,34 +1,54 @@
-const { v4 } = require('uuid')
-const AWS = require('aws-sdk')
+const { v4 } = require("uuid");
+const AWS = require("aws-sdk");
 
 const addProduct = async (event) => {
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-    const dynamodb = new AWS.DynamoDB.DocumentClient();
+  const {
+    name,
+    category,
+    provider,
+    price,
+    finalPrice,
+    discount,
+    stock,
+    description,
+    instructions,
+    targetedPublic,
+    content,
+  } = JSON.parse(event.body);
+  const createdAt = new Date();
+  const id = v4();
 
-    const { name, description, provider } = JSON.parse(event.body);
-    const createdAt = new Date();
-    const id = v4();
+  const newProduct = {
+    id,
+    name,
+    category,
+    provider,
+    price,
+    finalPrice,
+    discount,
+    stock,
+    description,
+    instructions,
+    targetedPublic,
+    content,
+    createdAt,
+  };
 
-    const newProduct = {
-        id,
-        name,
-        description,
-        provider,
-        createdAt
-    }
+  await dynamodb
+    .put({
+      TableName: "Products",
+      Item: newProduct,
+    })
+    .promise();
 
-    await dynamodb.put({
-        TableName: 'Products',
-        Item: newProduct
-    }).promise()
-
-    return {
-        status: 200,
-        body: JSON.stringify(newProduct)
-    }
-
-}
+  return {
+    status: 200,
+    body: JSON.stringify(newProduct),
+  };
+};
 
 module.exports = {
-    addProduct
-}
+  addProduct,
+};
